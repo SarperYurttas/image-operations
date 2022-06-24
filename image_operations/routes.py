134b87ce487065711e3cb29ac2ddd984e7d9254f
sarper <sarperyurttas36@gmail.com>
2022-06-24 15:32:21 +0300

    context object converted to session object

--- image_operations/routes.py
+++ image_operations/routes.py
@@ -1,6 +1,7 @@
 import os
+import secrets
 
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 
 from image_operations import app
 
@@ -8,30 +9,13 @@ from .operations import remove_background, resize_img
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-
-class Context(object):  # Singleton class
-    _instance = None
-    data = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Context, cls).__new__(cls)
-        return cls._instance
-
-    def error(self, msg='Unknown error!', logo=True):
-        self.data['error'] = msg
-        if logo:
-            self.data['img_name'] = 'io_logo.png'
-
-    def reset(self):
-        self.data = {
-            'error': None,
-            'img_name': 'io_logo.png',
-            'img_path': os.path.join(app.config['UPLOAD_FOLDER'], 'cached_img.png'),
-        }
+app.secret_key = 'gizli_sifre'
 
 
-context = Context()
+def raise_error(msg='Unknown error!', logo=True):
+    session['context']['error'] = msg
+    if logo:
+        session['context']['img_name'] = 'io_logo.png'
 
 
 def allowed_file(filename):
@@ -40,76 +24,83 @@ def allowed_file(filename):
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    if context.data is None:
+    if session['context']['img_name'] == 'io_logo.png':
         return redirect('/')
 
-    return render_template('result.html', context=context.data)
+    return render_template('result.html', context=session['context'])
 
 
 @app.route('/resize', methods=['GET', 'POST'])
 def resize():
-    if context.data is None:
+    if session['context']['img_name'] == 'io_logo.png':
         return redirect('/')
 
     if request.method == 'POST':
         if request.form.get('textbox') == ' ':
-            context.error(msg='Enter resize factor', logo=False)
-            return render_template('resize.html', context=context.data)
+            raise_error(msg='Enter resize factor', logo=False)
+            return render_template('resize.html', context=session['context'])
 
         elif request.form.get('textbox').strip().isnumeric():
             factor = int(request.form.get('textbox'))
             method = request.form.get('methods')
-            context.data['error'] = None
+            session['context']['error'] = None
 
             if factor > 4:
-                context.error(msg='Factor is too high', logo=False)
-                return render_template('resize.html', context=context.data)
+                raise_error(msg='Factor is too high (max: 4)', logo=False)
+                return render_template('resize.html', context=session['context'])
 
-            resize_img(context.data['img_path'], factor=factor, method=method)
-            context.data['img_name'] = 'cached_img_resized.png'
+            resize_img(session['context']['img_path'], factor=factor, method=method)
+            session['context']['img_name'] = 'cached_img_resized.png'
             return redirect('/result')
 
         else:
-            context.error(msg='Factor must be numeric', logo=False)
-            return render_template('resize.html', context=context.data)
+            raise_error(msg='Factor must be numeric', logo=False)
+            return render_template('resize.html', context=session['context'])
 
-    return render_template('resize.html', context=context.data)
+    return render_template('resize.html', context=session['context'])
 
 
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
-    if context.data is None:
+    if session['context']['img_name'] == 'io_logo.png':
         return redirect('/')
 
     if request.method == 'POST':
         if request.form.get('button1') == 'Remove Background':
-            remove_background(context.data['img_path'])
-            context.data['img_name'] = 'cached_img_bgremoved.png'
+            remove_background(session['context']['img_path'])
+            session['context']['img_name'] = 'cached_img_bgremoved.png'
             return redirect('/result')
 
         elif request.form.get('button2') == 'Resize':
             return redirect('/resize')
 
-    return render_template('menu.html', context=context.data)
+    return render_template('menu.html', context=session['context'])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    context.reset()
-
+    context = {
+        'error': None,
+        'is_file_uploaded': False,
+        'img_name': 'io_logo.png',
+        'img_path': os.path.join(app.config['UPLOAD_FOLDER'], 'cached_img.png'),
+    }
+    session['context'] = context
+    # app.logger.info(secrets.token_hex())
     if request.method == 'POST':
         if 'file' not in request.files:
-            context.error('Choose file first!')
-            return render_template('index.html', context=context.data)
+            raise_error('Choose file first!')
+            return render_template('index.html', context=session['context'])
 
         file = request.files['file']
         if file.filename == '':
-            context.error('Choose file first!')
-            return render_template('index.html', context=context.data)
+            raise_error('Choose file first!')
+            return render_template('index.html', context=session['context'])
 
         if file and allowed_file(file.filename):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'cached_img.png'))
-            context.data['img_name'] = 'cached_img.png'
+            session['context']['img_name'] = 'cached_img.png'
+            session['context']['is_file_uploaded'] = 'cached_img.png'
             return redirect('/menu')
 
-    return render_template('index.html', context=context.data)
+    return render_template('index.html', context=session['context'])
